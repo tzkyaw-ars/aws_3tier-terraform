ef34b98012403348b3417acc2d2619b9ba7cb3fa
TZK-hub <thantzinkyaw.3acc@gmail.com>
2023-09-26 22:08:38 +0630

    modified files

--- terraform/data.sh
+++ terraform/data.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+sudo yum update -y
+sudo yum upgrade -y
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
 
-yum update -y
-yum install -y httpd.x86_64
-systemctl start httpd.service
-systemctl enable httpd.service
-echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+echo "<html><head><title> Apache 2023 Terraform </title> </head><body><h1>Congratulations! YOU DID IT  <header><p> Completed by Mel Foster 07/09/2023 </p></body>! <h1><hr><article>  <p> Welcome Green Team  <header><p> Completed by Mel Foster 07/09/2023 </p></body> You successfully launched an AWS EC2 Instance with a Custom Apache webpage, and completed the WK22 Terraform Objectives! <p>  <header><p> Completed by Mel Foster 07/09/2023 </p></body> </html>" | sudo tee /var/www/html/index.html
--- terraform/rds.tf
+++ terraform/rds.tf
@@ -3,7 +3,8 @@ resource "aws_db_instance" "database" {
   allocated_storage = var.allocated_storage
   engine_version    = var.engine_version
   multi_az          = true
-  db_name           = var.db_name
+  db_name           = "primary" 
+#   identifier        = "database_primary"
   username          = var.rds_db_username
   password          = var.rds_db_password
   instance_class    = var.instance_class
@@ -13,18 +14,16 @@ resource "aws_db_instance" "database" {
   db_subnet_group_name   = aws_db_subnet_group.rds_subnets.name
 }
 
+resource "aws_db_instance" "database-replica" {
+  replicate_source_db         = "aws_db_instance.database.identifier"
+  replica_mode                = "mounted"
+  auto_minor_version_upgrade  = false
+  backup_retention_period     = 7
+  identifier                  = "instance-replica"
+  allocated_storage           = var.allocated_storage
+  instance_class              = var.instance_class
+  skip_final_snapshot         = true
+  vpc_security_group_ids      = [aws_security_group.database-sg.id]
+  db_subnet_group_name        = aws_db_subnet_group.rds_subnets.name
 
-resource "aws_db_instance" "database_replica" {
-  allocated_storage = var.allocated_storage
-  engine_version    = var.engine_version
-  multi_az          = true
-  db_name           = var.db_name
-  username          = var.rds_db_username
-  password          = var.rds_db_password
-  instance_class    = var.instance_class
-  engine            = var.db_engine
-  skip_final_snapshot = true
-  vpc_security_group_ids = [aws_security_group.database-sg.id]
-  db_subnet_group_name   = aws_db_subnet_group.rds_subnets.name
-  source_db_instance_identifier = aws_db_instance.database.id
 }
